42c32fca79441f655f5a57c5238c7efb1cca5167
luut189 <minhtuongluu74@gmail.com>
2022-11-23 20:00:44 -0500

    added MouseHandler to handle mouse input

---
+++ src/gui/MouseHandler.java
@@ -0,0 +1,27 @@
+package gui;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.SwingUtilities;
+
+import engine.Board;
+
+public class MouseHandler extends MouseAdapter {
+
+    Board board;
+
+    public MouseHandler(Board board) {
+        this.board = board;
+    }
+    
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        if(SwingUtilities.isLeftMouseButton(e)) {
+
+        } else if(SwingUtilities.isRightMouseButton(e)) {
+            
+        }
+    }
+
+}
