f72f608f8dae0d6939e8105862c0e21255b3cc40
luut189 <minhtuongluu74@gmail.com>
2022-11-24 15:59:00 -0500

    added code to select a square with mouse

--- src/gfx/MouseHandler.java
+++ src/gfx/MouseHandler.java
@@ -9,6 +9,24 @@ import engine.Board;
 
 public class MouseHandler extends MouseAdapter {
 
+    private class Position {
+        private int rank;
+        private int file;
+
+        Position(int rank, int file) {
+            this.rank = rank;
+            this.file = file;
+        }
+
+		public int getRank() {
+			return rank;
+		}
+
+		public int getFile() {
+			return file;
+		}
+    }
+
     private Board board;
 
     private Renderer render;
@@ -20,11 +38,17 @@ public class MouseHandler extends MouseAdapter {
         this.render = render;
         this.size = windowSize/8;
     }
+
+    public Position getPosition(MouseEvent e) {
+        return new Position(e.getY()/this.size, e.getX()/this.size);
+    }
     
     @Override
     public void mouseClicked(MouseEvent e) {
         if(SwingUtilities.isLeftMouseButton(e)) {
-
+            Position pos = getPosition(e);
+            this.board.setSelectedStartPosition(pos.getRank(), pos.getFile());
+            this.render.repaint();
         } else if(SwingUtilities.isRightMouseButton(e)) {
             
         }
